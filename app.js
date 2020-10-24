12053f9c9ddf4f105e9aa247b281e6f8bdde6e98
Myles Burt <myles_burt@rocketmail.com>
2020-10-24 18:14:08 +1030

    user prompt function for adding team member.

--- app.js
+++ app.js
@@ -10,6 +10,24 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+function userPrompt() {
+    return inquirer.prompt([
+        {
+            type: "list",
+            name: "teamMember",
+            message: "Which type of team member would you like to add?",
+            choices: [
+                "Engineer",
+                "Intern",
+                "I don't want to add any more team members",
+            ],
+        },
+    ]);
+}
+
+
+
+
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
